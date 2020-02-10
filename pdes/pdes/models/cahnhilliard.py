fix gradients

pdes/pdes/models/cahnhilliard.py
@@ -12,9 +12,14 @@ from ..ops import LaplacianFilter
 class _AbstractCahnHilliard(_AbstractPDE):
     def __init__(self,params,dx=5e2,M=1e8,padding_mode='circular',device='cpu'):
         super(_AbstractCahnHilliard, self).__init__()
-        self._params = params
+        #self._params = params
+        self._assign_params(params)
         self._M      = M
         self._lapl   = LaplacianFilter(dx, padding_mode, device)
+
+    @abstractmethod
+    def _assign_params(self, _params):
+        NotImplemented
 
     @abstractmethod
     def _dF_dphi(self, phi):
@@ -25,9 +30,14 @@ class _AbstractCahnHilliard(_AbstractPDE):
         return self._M*self._lapl(mu)
 
 class LandauCahnHilliard(_AbstractCahnHilliard):
+    def _assign_params(self, _params):
+        self._a = torch.nn.Parameter(_params['a'])
+        self._b = torch.nn.Parameter(_params['b'])
+        self._k = torch.nn.Parameter(_params['k'])
+
     def _dF_dphi(self, phi):
-        a,b,k = [self._params[k].view(-1,*[1]*3) for k in ('a','b','k')]
-        return -a*phi + b*torch.pow(phi, 3) - k*self._lapl(phi)
+        #a,b,k = [self._params[k].view(-1,*[1]*3) for k in ('a','b','k')]
+        return -self._a*phi + self._b*torch.pow(phi, 3) - self._k*self._lapl(phi)
 
 class FloryHugginsCahnHilliard(_AbstractCahnHilliard):
     def _dF_dphi(self, phi):

pdes/pdes/ops.py
@@ -5,7 +5,7 @@ import numpy as np
 from torchdiffeq import odeint_adjoint as odeint
 
 from torchfilter.util import safe_cast
-
+# 
 # class LaplacianFilter(torch.nn.Conv2d):
 #     def __init__(self, dx = 1, padding_mode='circular', device='cpu'):
 #         super(LaplacianFilter, self).__init__(
@@ -15,7 +15,7 @@ from torchfilter.util import safe_cast
 #             bias=False, padding=2
 #         )
 #         #self.weight.detach()
-#         #self.weight.requires_grad=False
+#         self.weight.requires_grad=False
 #         self.weight[0,0,:,:] = torch.tensor(
 #             [[0., 1., 0.,], [1., -4., 1.], [0., 1., 0.]]
 #         )/(dx**2)